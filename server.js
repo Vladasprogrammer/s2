e8da6ca92407a13354d2b53b85ff6d91105ca146
Vladasprogrammer <voroninvlad3@gmail.com>
2025-02-24 21:58:06 +0200

    Updates from the lecture

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
- 
+const md5 = require('md5');
+const uuid = require('uuid');
+const cookieParser = require('cookie-parser');
+
 const URL = 'http://localhost:5173';
- 
+
+
+app.use(cookieParser());
+
 app.use(cors(
     {
         origin: URL,
         credentials: true
     }
 ));
- 
+
 app.use(express.json());
- 
+
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -24,7 +30,7 @@ const con = mysql.createConnection({
 app.post('/login', (req, res) => {
     const { name, password } = req.body;
     const sql = 'SELECT * FROM users WHERE name = ? AND password = ?';
-    con.query(sql, [name, password], (err, result) => {
+    con.query(sql, [name, md5(password)], (err, result) => {
         if (err) {
             res.status(500).send('Klaida bandant prisijungti');
             return;
@@ -33,11 +39,52 @@ app.post('/login', (req, res) => {
             res.status(401).send('Neteisingi prisijungimo duomenys');
             return;
         }
-        res.status(200).send('Prisijungimas sėkmingas');
+        const token = uuid.v4();
+        const updateSql = 'UPDATE users SET session_id = ? WHERE name = ?';
+        con.query(updateSql, [token, name], (err) => {
+            if (err) {
+                res.status(500).send('Klaida bandant prisijungti');
+                return;
+            }
+
+            res.cookie('r2-token', token, { httpOnly: true, SameSite: 'none' });
+            res.status(200).json({
+                success: true,
+                message: 'Prisijungimas sėkmingas',
+                user: {
+                    role: result[0].role,
+                    name: result[0].name,
+                    id: result[0].id
+                }
+            });
+        });
     });
- 
 });
- 
+
+app.get('/get-user', (req, res) => {
+    const token = req.cookies['r2-token'] || '';
+    const sql = 'SELECT * FROM users WHERE session_id = ?';
+    con.query(sql, [token], (err, result) => {
+        if (err) {
+            res.status(500).send('Klaida bandant prisijungti');
+            return;
+        }
+        if (result.length === 0) {
+            res.status(200).json({ 
+                role: 'guest',
+                name: 'Guest',
+                id: 0,
+             });
+            return;
+        }
+        res.status(200).json({
+            role: result[0].role,
+            name: result[0].name,
+            id: result[0].id
+        });
+    })
+})
+
 con.connect(err => {
     if (err) {
         console.log('Klaida prisijungiant prie DB');
@@ -45,4 +92,9 @@ con.connect(err => {
     }
     console.log('Prisijungimas prie DB buvo sėkmingas');
 });
- 
+
+
+const port = 3333;
+app.listen(port, () => {
+    console.log(`Serveris pasiruošęs ir laukia ant ${port} porto!`);
+});
